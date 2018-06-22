8dc1a02d8261a7e03e5648b12c700dcbc7868b7b
Padmanabhan <padmanabhan.skp@gmail.com>
2018-06-22 12:54:16 +0100

    Add files via upload

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int iCount = 1; iCount <= 100; iCount++)
+            {
+                var result = (iCount % 3 == 0 && iCount % 5 == 0) ? "FizzBuzz"
+                    : (iCount % 5 == 0) ? "Buzz" : (iCount % 3 == 0) ? "Fizz" : iCount.ToString();
+
+                Console.WriteLine(result);
+            }
+
+            Console.Read();
+        }
+    }
+}
